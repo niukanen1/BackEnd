ddece9b93d3586ef90ec83996718b1752a80b63d
Ivan Niukanen <ivanniu2003@gmail.com>
2023-01-16 22:44:22 +0200

    testing

--- index.ts
+++ index.ts
@@ -1,68 +1,69 @@
-import { usersCollection } from './databaseConnector';
-import { AddUserToDb, LoginUser, User } from './Helpers/User/UserService';
-import express from "express"; 
-import dotenv from 'dotenv'; 
-import { Authenticate, GenerateToken } from './Helpers/Token/TokenService';
-import { ResponseObject } from './Helpers/Response/Response';
-import cookieParser from 'cookie-parser';
-import { protectedRouter } from './routes/Protected/protectedRoute';
-import cors from 'cors';
-dotenv.config()
+import { usersCollection } from "./databaseConnector";
+import { AddUserToDb, LoginUser, User } from "./Helpers/User/UserService";
+import express from "express";
+import dotenv from "dotenv";
+import { Authenticate, GenerateToken } from "./Helpers/Token/TokenService";
+import { ResponseObject } from "./Helpers/Response/Response";
+import cookieParser from "cookie-parser";
+import { protectedRouter } from "./routes/Protected/protectedRoute";
+import cors from "cors";
+dotenv.config();
 
-const app = express(); 
-const corsOptions ={
-    origin:'*', 
-    // credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
-
+const app = express();
+const corsOptions = {
+	origin: "http://localhost:3000",
+	credentials: true,
+};
 
 app.use(express.json());
-app.use(express.urlencoded())
-app.use(cookieParser())
+app.use(express.urlencoded());
+app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(Authenticate);
 app.use("/protected", protectedRouter);
 
-export interface TypedRequest<T> extends Express.Request { 
-    body: T
+export interface TypedRequest<T> extends Express.Request {
+	body: T;
 }
 
 app.post("/register", async (req: TypedRequest<User>, res) => {
-    const user = req.body;
+	const user = req.body;
 
-    const token = GenerateToken(user, "15m");
-    const userAddingToDbCheck = await AddUserToDb(user)
-    if (!userAddingToDbCheck.success) { 
-        return res.status(400).json(userAddingToDbCheck)
-    }
-    return res.status(200).cookie("accessToken", token, { httpOnly: true, secure: false}).json(new ResponseObject("Successfully registered", true));
+	const token = GenerateToken(user, "15m");
+	const userAddingToDbCheck = await AddUserToDb(user);
+	if (!userAddingToDbCheck.success) {
+		return res.status(400).json(userAddingToDbCheck);
+	}
+	return res
+		.status(200)
+		.cookie("accessToken", token, { httpOnly: true, secure: false })
+		.json(new ResponseObject("Successfully registered", true));
+});
 
-})
+app.post("/login", async (req: TypedRequest<User>, res) => {
+	const user = req.body;
 
-app.post("/login", async (req: TypedRequest<User>, res) => { 
-    const user = req.body; 
+	try {
+		const userCheck = await LoginUser(user);
+		if (!userCheck.success) {
+			return res.status(403).json(userCheck);
+		}
+	} catch (err) {
+		console.log(err);
+	}
 
-    try { 
-        const userCheck = await LoginUser(user)
-        if (!userCheck.success) { 
-            return res.status(403).json(userCheck); 
-        }
+	const token = GenerateToken(user, "15m");
 
-    } catch(err) { 
-        console.log(err)
-    }
+	return res
+		.status(200)
+		.cookie("accessToken", token, { httpOnly: true, secure: false })
+		.json(new ResponseObject("Successfully logged in", true));
+});
 
-    const token = GenerateToken(user, '15m'); 
+app.get("/protected/someData", Authenticate, async (req, res) => {
+	return res.status(200).json(new ResponseObject("Success", true, { array: ["1", "2", "3"] }));
+});
 
-    return res.status(200).cookie("accessToken", token, {httpOnly: true, secure: false}).json(new ResponseObject("Successfully logged in", true)); 
-
-})
-
-app.get("/protected/someData", Authenticate, async (req, res) => { 
-    return res.status(200).json(new ResponseObject("Success", true, {array: ["1", "2", "3"]}))
-})
-
-app.listen(3000, () => { 
-    console.log("Started server")
-})
+app.listen(3000, () => {
+	console.log("Started server");
+});
