a446e0bac0ed4f299fbaf6147246a96fe4ba3f8b
Ivan Niukanen <ivanniu2003@gmail.com>
2023-01-17 17:54:52 +0200

    cleaning

--- Helpers/Token/TokenService.ts
+++ Helpers/Token/TokenService.ts
@@ -2,8 +2,8 @@ import jwt from "jsonwebtoken";
 import { User } from "../User/UserService";
 import { NextFunction, Request, Response } from "express";
 import { ResponseObject } from "../Response/Response";
-import { SerializeToken, TypedRequest } from "../..";
 import { usersCollection } from "../../databaseConnector";
+import { serialize } from "cookie";
 
 const SECRET_KEY = process.env.SECRET_KEY ?? "another_Secret_dont_tell_anybody_pls";
 
@@ -14,15 +14,8 @@ export function GenerateToken( user: User, expiration: string | number = '1m' ) 
 
 export async function Authenticate(req: Request, res: Response, next: NextFunction) { 
     if (!req.path.includes("/protected")) return next(); 
-    console.log("AUTH");
-    console.log(req.body);
-    console.log(req.header);
-    console.log(req.headers);
-    
-    const {accessToken: token} = req.cookies; 
-    console.log(req.cookies);
 
-    console.log("accessToken: " + token);
+    const {accessToken: token} = req.cookies; 
     
     if (!token) { 
         return res.status(403).json(new ResponseObject("Token should be provided!", false));
@@ -34,22 +27,18 @@ export async function Authenticate(req: Request, res: Response, next: NextFuncti
         req.body.user = fullUser; 
         res.setHeader('Set-Cookie', SerializeToken(GenerateToken({ email: decoded.email, password: decoded.password}, '15m')));
         res.status(200)
-        // .cookie("accessToken", GenerateToken({ email: decoded.email, password: decoded.password}, '15m'), {httpOnly: true, secure: false, sameSite: 'none'});
-        
-        
-        // jwt.verify(token, SECRET_KEY, (err: any, user: any) => { 
-        //     if (err != undefined) { 
-        //         console.log("error")
-        //         return res.status(403).send(new ResponseObject("Token is not valid", false));
-        //     }
-        //     console.log(user)
-        //     // res.status(200).cookie("accessToken", GenerateToken(user, '1h'), {httpOnly: true, secure: false});
-        //     req.body.user = user;
-        // });
     }
     catch (err) { 
-        // console.log((err as Error));
         return res.status(403).json(new ResponseObject((err as Error).message, false));
     }
     next()
+}
+
+
+export function SerializeToken(token: string) { 
+    return serialize('accessToken', token, {
+        httpOnly: true, 
+        secure: true, 
+        sameSite: "none",
+    })
 }
--- routes/Protected/protectedRoute.ts
+++ routes/Protected/protectedRoute.ts
@@ -4,9 +4,6 @@ import { movieRouter } from "./Movies/MovieRouter";
 import { UserRouter } from "./User/UserRouter";
 
 export const protectedRouter: Router = express.Router(); 
-protectedRouter.post("/logout", async (req, res) => {
-    return res.clearCookie("accessToken").status(200).json(new ResponseObject("Successfully logged out", true)); 
-})
 
 protectedRouter.use("/movies", movieRouter);
 protectedRouter.use("/user", UserRouter);
